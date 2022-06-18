b3ad6e3b3959adf157eae69e493764d8031c9d59
garik1988 <garikmoutafalidis@gmail.com>
2022-06-18 23:11:58 +0300

    updated code to a cleaner version with a bunch of tests to avoid duplicates

--- macrand.py
+++ macrand.py
@@ -1,8 +1,8 @@
 #! usr/bin/env python
 import subprocess
 from randmac import RandMac
-from get_nic import getnic
 from optparse import OptionParser
+import re
 
 
 
@@ -16,28 +16,62 @@ def generate_mac(): #random mac address generator
     return str(generated_mac)
 
 def check_interface(interface): #checks if user's entered interface exists if not lists availamble interfaces
-    interfaces = getnic.interfaces() #makes a list of availamble interfaces
+    ifconfig_output = subprocess.check_output(["ifconfig"]).decode("utf-8") #creates multine string from ifconfig output in utf-8 encoding
 
-    if interface not in interfaces: #checks if user's entered interface exists
-        print ("+ There is no such interface available")
-        print("Available Interfaces: \n")
-        for i in range(len(interfaces)):  # prints available interfaces
-            print(str(i) + ")", interfaces[i], "\n")
-    else:
+    interfaces = re.findall(r"^\w+", ifconfig_output, re.MULTILINE) #makes a list of available interfaces
+
+    if  interface  in interfaces: #checks if user's entered interface exists
         change_mac(interface)
+    else:
+
+        print ("+ There is no such interface available")
+
+        print("Available Interfaces: \n")
+
+        for i in range(len(interfaces)):  # prints available interfaces
+
+            print(str(i) + ")", interfaces[i], "\n")
+
+def ifconfig_output_capture_mac(interface): #captures current mac address of interface
+    ifconfig_output = subprocess.check_output(["ifconfig", interface]).decode("utf-8")
+
+    current_mac = re.findall(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_output, re.MULTILINE)
+
+    return current_mac[0]
 
 def change_mac(interface):
+
         new_mac=generate_mac()
 
         print ("+ Changing Mac Address for "+interface+" to "+new_mac)
 
-        subprocess.call("ifconfig " + interface + " down", shell=True)
+        if new_mac == ifconfig_output_capture_mac(interface): #checks if the new random mac is different from the one trying to change if it is its generates new one
 
-        subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
+            new_mac = generate_mac()
 
+            subprocess.call("ifconfig " + interface + " down", shell=True)
+
+            subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
+
+        elif new_mac != ifconfig_output_capture_mac(interface):
+
+            subprocess.call("ifconfig " + interface + " down", shell=True)
+
+            subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
         subprocess.call("ifconfig " + interface + " up", shell=True)
-        
+
+        if new_mac == ifconfig_output_capture_mac(interface) :
+
+            print("+ Mac address changed succesfully to"+new_mac)
+        else:
+
+            print ("- Mac address failed to change")
+
+
+
+
 def get_arguments():
+
     parser = OptionParser()
 
     parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAc address")
@@ -51,3 +85,4 @@ def get_arguments():
 
 
 options=get_arguments()
+
